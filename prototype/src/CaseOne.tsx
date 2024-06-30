76357123a1a4bf3bb29ba5c3d930aedcd1f2757e
wangqianwen0418 <wangqianwen0418@163.com>
2024-06-29 23:45:22 -0500

    add router

--- prototype/src/CaseOne.tsx
+++ prototype/src/CaseOne.tsx
@@ -7,12 +7,27 @@ import Interpretation from './component/Interpretation';
 
 import './App.css';
 
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Toolbar } from '@mui/material';
+
 function App() {
   const [isSubmitted, setIsSubmitted] = useState(false);
   return (
     <Grid container>
     <Grid item xs={12}>
-      <header className="App-header">Are you interpreting correctly?</header>
+      <Toolbar sx={{backgroundColor: 'black', fontSize: '28px', color: 'white'}}>
+      <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            // onClick={handleDrawerOpen}
+            edge="start"
+            // sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        Are you interpreting correctly?
+      </Toolbar>
     </Grid>
     <Grid item xs={4} className='App-body'>
       <Interpretation isSubmitted={isSubmitted} setIsSubmitted={setIsSubmitted}/>
--- prototype/src/index.tsx
+++ prototype/src/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CaseOne from './CaseOne';
+import CaseTwo from './CaseTwo';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CaseOne />,
+  },
+  {
+    path: "CaseTwo",
+    element: <CaseTwo />,
+  },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
