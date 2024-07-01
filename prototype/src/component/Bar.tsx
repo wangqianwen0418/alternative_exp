1aa6ec373b61a40b33052c6748e412de8b70422a
wangqianwen0418 <wangqianwen0418@163.com>
2024-06-30 19:06:56 -0500

    fix bar chart bug

--- prototype/src/component/Bar.tsx
+++ prototype/src/component/Bar.tsx
@@ -15,14 +15,16 @@ export default function Bar(props: BarProps) {
 
     const { allShapValues, featureNames, height, width, id, offsets } = props;
     let avgShapeValues: { [featureName: string]: number } = {}
-    for (let i = 0; i < allShapValues.length; i++) {
+    for (let i = 0; i < featureNames.length; i++) {
         avgShapeValues[featureNames[i]] = allShapValues.map((val, index) => Math.abs(val[i])).reduce((a, b) => a + b, 0) / allShapValues.length
     }
 
-    let sortedAvgShapeValues = Object.entries(avgShapeValues).sort((a, b) => a[1] - b[1])
+    let sortedAvgShapeValues = Object.entries(avgShapeValues).sort((a, b) => a[1] - b[1]).reverse()
 
-    const yScale = d3.scaleBand().domain(featureNames).range([margin[1], height - margin[3]]).padding(0.1)
-    const xScale = d3.scaleLinear().domain([0, d3.max(sortedAvgShapeValues, d => d[1]) as number]).range([margin[0], width - margin[2]])
+    console.info(sortedAvgShapeValues)
+
+    const yScale = d3.scaleBand().domain(sortedAvgShapeValues.map(d => d[0])).range([margin[1], height - margin[3] - margin[1]]).padding(0.1)
+    const xScale = d3.scaleLinear().domain([0, d3.max(sortedAvgShapeValues, d => d[1]) as number]).range([margin[0], width - margin[2] - margin[0]])
 
 
     return <g
@@ -30,10 +32,12 @@ export default function Bar(props: BarProps) {
         id={id}
         transform={`translate(${offsets[0]}, ${offsets[1]})`}
     >
+        <rect className='background' width={width} height={height} fill='white' stroke='gray' />
+
         <g className='bars'>
             {sortedAvgShapeValues.map(([featureName, value], index) => {
                 return <g k={featureName}>
-                    <text y={yScale(featureName) as number + yScale.bandwidth() * 0.8} >{featureName}</text>
+                    <text x={margin[0] - 2} y={yScale(featureName) as number + yScale.bandwidth() * 0.8} textAnchor='end'>{featureName}</text>
                     <rect
                         key={featureName}
                         x={xScale(0)}
