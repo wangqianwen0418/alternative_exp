6b2c8b28b51a1fa0e3dc1180f08966456458b3cb
wangqianwen0418 <wangqianwen0418@163.com>
2024-07-03 17:48:07 -0500

    update case data

--- prototype/src/const/index.ts
+++ prototype/src/const/index.ts
@@ -1,26 +1,27 @@
 export const CASES = [
-    {
-        name: "Case 1",
-        href: "/case1",
-        userText: '',
-        initVis: "beeswarm"
-    },
-    {
-        name: "Case 2",
-        href: "/case2",
-        userText: 'serum triglycerides level is the most important feature for predicting the progression of diabetes.',
-        initVis: "bar"
-    },
-    {
-        name: "Case 3",
-        href: "/case3",
-        userText: '',
-        initVis: "scatter"
-    },
-    {
-        name: "Free Exploration",
-        href: "/free",
-        userText: '',
-        initVis: "random"
-    }
-]
+  {
+    name: "Case 1",
+    href: "/case1",
+    userText: "a high bmi leads to large diabete progression",
+    initVis: "beeswarm",
+  },
+  {
+    name: "Case 2",
+    href: "/case2",
+    userText:
+      "serum triglycerides level is the most important feature for predicting the progression of diabetes.",
+    initVis: "bar",
+  },
+  {
+    name: "Case 3",
+    href: "/case3",
+    userText: "",
+    initVis: "scatter",
+  },
+  {
+    name: "Free Exploration",
+    href: "/free",
+    userText: "",
+    initVis: "random",
+  },
+];
