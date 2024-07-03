515ba8ed727bc38aaab3e660bed894e9a58f1d2e
wangqianwen0418 <wangqianwen0418@163.com>
2024-07-03 17:34:03 -0500

    add interaction to pcp

--- prototype/src/component/Explanation.tsx
+++ prototype/src/component/Explanation.tsx
@@ -6,135 +6,137 @@ import Scatter from "./Scatter";
 import Bar from "./Bar";
 import PCP from "./PCP";
 import { IHypo } from "../App";
-import { CASES } from '../const';
+import { CASES } from "../const";
 
-type props = typeof CASES[0] & {
-    isSubmitted: boolean;
-    hypo: IHypo | undefined
-}
+type props = (typeof CASES)[0] & {
+  isSubmitted: boolean;
+  hypo: IHypo | undefined;
+};
 
 export default function Explanation(props: props) {
-    const { isSubmitted, initVis } = props;
-    const featureName = "bmi",
-        featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
-        featureValues = shap_diabetes["feature_values"].map(
-            (row) => row[featureIndex]
-        ),
-        featureShapValues = shap_diabetes["shap_values"].map((row) => row[featureIndex]);
+  const { isSubmitted, initVis } = props;
+  const featureName = "bmi",
+    featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
+    featureValues = shap_diabetes["feature_values"]
+      .slice(0, 100)
+      .map((row) => row[featureIndex]),
+    featureShapValues = shap_diabetes["shap_values"]
+      .slice(0, 100)
+      .map((row) => row[featureIndex]);
 
+  let initialVisualization;
+  switch (initVis) {
+    case "beeswarm":
+      initialVisualization = (
+        <Swarm
+          xValues={featureShapValues}
+          colorValues={featureValues}
+          width={500}
+          height={100}
+          id="bmi"
+        />
+      );
+      break;
+    case "scatter":
+      initialVisualization = (
+        <Scatter
+          yValues={featureShapValues}
+          xValues={featureValues}
+          width={400}
+          height={300}
+          id="bmi-scatter"
+          offsets={[0, 0]}
+        />
+      );
+      break;
+    case "bar":
+      initialVisualization = (
+        <Bar
+          allShapValues={shap_diabetes["shap_values"].slice(0, 100)}
+          featureNames={shap_diabetes["feature_names"].slice(0, 100)}
+          width={600}
+          height={200}
+          id="bmi-scatter"
+          offsets={[0, 0]}
+        />
+      );
+      break;
+    default:
+      initialVisualization = (
+        <Swarm
+          xValues={featureShapValues}
+          colorValues={featureValues}
+          width={500}
+          height={100}
+          id="bmi"
+        />
+      );
+  }
 
-    let initialVisualization;
-    switch (initVis) {
-        case "beeswarm":
-            initialVisualization = (
-                <Swarm
-                    xValues={featureShapValues}
-                    colorValues={featureValues}
-                    width={500}
-                    height={100}
-                    id="bmi"
-                />
-            );
-            break;
-        case "scatter":
-            initialVisualization = (
-                <Scatter
-                    yValues={featureShapValues}
-                    xValues={featureValues}
-                    width={400}
-                    height={300}
-                    id="bmi-scatter"
-                    offsets={[0, 0]}
-                />
-            );
-            break;
-        case "bar":
-            initialVisualization = (
-                <Bar
-                    allShapValues={shap_diabetes["shap_values"]}
-                    featureNames={shap_diabetes["feature_names"]}
-                    width={600}
-                    height={200}
-                    id="bmi-scatter"
-                    offsets={[0, 0]}
-                />
-            );
-            break;
-        default:
-            initialVisualization = (
-                <Swarm
-                    xValues={featureShapValues}
-                    colorValues={featureValues}
-                    width={500}
-                    height={100}
-                    id="bmi"
-                />)
-    }
+  // [TODO: additional visualizations should be updated based in hypothesis]
+  let additionalVisualizations = (
+    <>
+      <Scatter
+        yValues={featureShapValues}
+        xValues={featureValues}
+        width={400}
+        height={300}
+        id="bmi-scatter"
+        offsets={[0, 150]}
+      />
 
-    // [TODO: additional visualizations should be updated based in hypothesis]
-    let additionalVisualizations = (
-        <>
-            <Scatter
-                yValues={featureShapValues}
-                xValues={featureValues}
-                width={400}
-                height={300}
-                id="bmi-scatter"
-                offsets={[0, 150]}
-            />
+      <g>
+        <rect
+          x={550}
+          y={0}
+          width={300}
+          height={200}
+          fill="white"
+          stroke="black"
+        />
+        <text x={700} y={50} textAnchor="middle">
+          Other Explanations
+        </text>
+      </g>
+      <g>
+        <rect
+          x={450}
+          y={250}
+          width={400}
+          height={200}
+          fill="white"
+          stroke="black"
+        />
+        <text x={600} y={300} textAnchor="middle">
+          Other Explanations
+        </text>
+      </g>
+    </>
+  );
 
-            <g>
-                <rect
-                    x={550}
-                    y={0}
-                    width={300}
-                    height={200}
-                    fill="white"
-                    stroke="black"
-                />
-                <text x={700} y={50} textAnchor="middle">
-                    Other Explanations
-                </text>
-            </g>
-            <g>
-                <rect
-                    x={450}
-                    y={250}
-                    width={400}
-                    height={200}
-                    fill="white"
-                    stroke="black"
-                />
-                <text x={600} y={300} textAnchor="middle">
-                    Other Explanations
-                </text>
-            </g>
-        </>
-    )
-
-    if (props.name === "Case 2") {
-        additionalVisualizations = (
-            <PCP
-                allShapValues={shap_diabetes["shap_values"]}
-                featureNames={shap_diabetes["feature_names"]}
-                width={600}
-                height={300}
-                id="pcp"
-                offsets={[0, 250]}
-            />
-        )
-    }
-
-    return (
-        <Paper style={{ padding: "15px" }}>
-            <Typography variant="h5" gutterBottom>
-                Visual Explanation
-            </Typography>
-            <svg className="swarm" width={900} height='70vh'>
-                {initialVisualization}
-
-                {isSubmitted && additionalVisualizations}
-            </svg>
-        </Paper >
+  if (props.name === "Case 2") {
+    additionalVisualizations = (
+      <PCP
+        allShapValues={shap_diabetes["shap_values"].slice(0, 100)}
+        featureNames={shap_diabetes["feature_names"]}
+        width={600}
+        height={300}
+        id="pcp"
+        offsets={[0, 250]}
+      />
     );
+  }
+
+  return (
+    <Paper style={{ padding: "15px" }}>
+      <Typography variant="h5" gutterBottom>
+        Visual Explanation
+      </Typography>
+      <svg className="swarm" width={900} height="70vh">
+        {initialVisualization}
+
+        {isSubmitted && additionalVisualizations}
+      </svg>
+    </Paper>
+  );
 }
--- prototype/src/component/Interpretation.tsx
+++ prototype/src/component/Interpretation.tsx
@@ -1,22 +1,22 @@
 import {
-    Button,
-    Paper,
-    TextField,
-    Typography,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    CircularProgress
+  Button,
+  Paper,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  CircularProgress,
 } from "@mui/material";
 import "./Interpretation.css";
 import { useState } from "react";
-import openai from '../openai_config.js';
+import openai from "../openai_config.js";
 import shapData from "../assets/shap_diabetes.json";
 import { IHypo } from "../App";
-import { CASES } from '../const';
+import { CASES } from "../const";
 
-var response = ""
+var response = "";
 
 const feature_names = shapData.feature_names;
 
@@ -24,95 +24,96 @@ let feature_name_str = feature_names.join(", ");
 
 const prediction_data = shapData.prediction_name;
 
-
-
 function getSelection(
-    label: string,
-    value: string,
-    handleChange: (k: string) => void,
-    options: string[]
+  label: string,
+  value: string,
+  handleChange: (k: string) => void,
+  options: string[]
 ) {
-    return (
-        <FormControl
-            variant="standard"
-            sx={{ m: 1, minWidth: 120, display: "block" }}
-        >
-            {/* <InputLabel id="demo-simple-select-standard-label">{label}</InputLabel> */}
-            <Select
-                labelId="demo-simple-select-standard-label"
-                id="demo-simple-select-stelandard"
-                value={value}
-                onChange={(e) => handleChange(e.target.value)}
-                label="Age"
-            >
-                {options.map((option) => (
-                    <MenuItem key={option} value={option}>
-                        {option}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
-    );
+  return (
+    <FormControl
+      variant="standard"
+      sx={{ m: 1, minWidth: 120, display: "block" }}
+    >
+      {/* <InputLabel id="demo-simple-select-standard-label">{label}</InputLabel> */}
+      <Select
+        labelId="demo-simple-select-standard-label"
+        id="demo-simple-select-stelandard"
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+        label="Age"
+      >
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 }
 
 function formatText(
-    text: string,
-    entityType: "feature" | "relation" | "prediction" | "condition"
+  text: string,
+  entityType: "feature" | "relation" | "prediction" | "condition"
 ) {
-    if (entityType === "feature") {
-        return (
-            <span className="label" style={{ backgroundColor: "#6bbcff" }}>
-                {text}
-            </span>
-        );
-    } else if (entityType === "relation") {
-        return (
-            <span className="label" style={{ backgroundColor: "green" }}>
-                {text}
-            </span>
-        );
-    } else if (entityType === "prediction") {
-        return (
-            <span className="label" style={{ backgroundColor: "#ffc760" }}>
-                {text}
-            </span>
-        );
-    } else if (entityType === "condition") {
-        return (
-            <span className="label" style={{ backgroundColor: "#ffc760" }}>
-                {text}
-            </span>
-        );
-    }
+  if (entityType === "feature") {
+    return (
+      <span className="label" style={{ backgroundColor: "#6bbcff" }}>
+        {text}
+      </span>
+    );
+  } else if (entityType === "relation") {
+    return (
+      <span className="label" style={{ backgroundColor: "green" }}>
+        {text}
+      </span>
+    );
+  } else if (entityType === "prediction") {
+    return (
+      <span className="label" style={{ backgroundColor: "#ffc760" }}>
+        {text}
+      </span>
+    );
+  } else if (entityType === "condition") {
+    return (
+      <span className="label" style={{ backgroundColor: "#ffc760" }}>
+        {text}
+      </span>
+    );
+  }
 }
 
-type props = typeof CASES[0] & {
-    isSubmitted: boolean;
-    setIsSubmitted: (k: boolean) => void;
-    hypo: IHypo | undefined;
-    setHypo: (k: IHypo) => void;
-}
+type props = (typeof CASES)[0] & {
+  isSubmitted: boolean;
+  setIsSubmitted: (k: boolean) => void;
+  hypo: IHypo | undefined;
+  setHypo: (k: IHypo) => void;
+};
 
 export default function Interpretation(props: props) {
-    const { isSubmitted, setIsSubmitted, hypo, setHypo, userText } = props;
-    const [userInput, setUserInput] = useState(userText || 'e.g., a high bmi leads to large diabete progression');
-    const [parsedData, setParsedData] = useState({
-        feature: "aa",
-        relation: "aa",
-        prediction: "aa",
-        condition: "aa",
-        PossibleRelationships: ["aa", "bb"],
-        PossibleConditions: ["cc", "dd"]
-    });
-    const [selectedRelation, setSelectedRelation] = useState(parsedData.relation);
-    const [selectedCondition, setSelectedCondition] = useState(parsedData.condition);
-    const [isLoading, setIsLoading] = useState(false);  // New loading state
+  const { isSubmitted, setIsSubmitted, hypo, setHypo, userText } = props;
+  const [userInput, setUserInput] = useState(
+    userText || "e.g., a high bmi leads to large diabete progression"
+  );
+  const [parsedData, setParsedData] = useState({
+    feature: "aa",
+    relation: "aa",
+    prediction: "aa",
+    condition: "aa",
+    PossibleRelationships: ["aa", "bb"],
+    PossibleConditions: ["cc", "dd"],
+  });
+  const [selectedRelation, setSelectedRelation] = useState(parsedData.relation);
+  const [selectedCondition, setSelectedCondition] = useState(
+    parsedData.condition
+  );
+  const [isLoading, setIsLoading] = useState(false); // New loading state
 
-    const parseInput = async (input: string) => {
-        setIsLoading(true);  // Start loading
+  const parseInput = async (input: string) => {
+    setIsLoading(true); // Start loading
 
-
-        const prompt = `You are a bot that extracts the sentence structure from explanation sentences. You produce JSON that contains the following features from an inputted sentence: \:
+    const prompt = `You are a bot that extracts the sentence structure from explanation sentences. You produce JSON that contains the following features from an inputted sentence: \:
     Feature
     Prediction
     Relationship
@@ -154,128 +155,129 @@ export default function Interpretation(props: props) {
     If any of these values are missing in the sentence, the value for that field should be NONE. 
     
 
-    `
+    `;
 
-        const chatCompletion = await openai.chat.completions.create({
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      response_format: { type: "json_object" },
+      max_tokens: 1024,
+      messages: [
+        { role: "system", content: prompt },
+        { role: "user", content: input },
+      ],
+    });
+    console.log(`RESPONSE: '${chatCompletion.choices[0].message.content}'`);
 
+    // [TODO: @aninuth call setHypo here to update the hypo based the gpt response]
+    var json_string = chatCompletion.choices[0].message.content;
+    var finish_reason = chatCompletion.choices[0].finish_reason;
+    if (json_string !== null && finish_reason === "stop") {
+      try {
+        let jsonObject = JSON.parse(json_string);
 
-            model: "gpt-4o",
-            response_format: { "type": "json_object" },
-            max_tokens: 1024,
-            messages: [
-                { "role": "system", "content": prompt },
-                { "role": "user", "content": input }
-            ]
+        // Extract each element and store it in a variable
+        let feature = jsonObject.Feature;
+        let prediction = jsonObject.Prediction;
+        let relationship = jsonObject.Relationship;
+        let condition = jsonObject.Condition;
+        let PossibleRelationships = jsonObject.PossibleRelationships;
+        let PossibleConditions = jsonObject.PossibleConditions;
+        PossibleRelationships.push(relationship);
 
-        });
-        console.log(`RESPONSE: '${chatCompletion.choices[0].message.content}'`);
-
-        // [TODO: @aninuth call setHypo here to update the hypo based the gpt response]
-        var json_string = chatCompletion.choices[0].message.content;
-        var finish_reason = chatCompletion.choices[0].finish_reason;
-        if (json_string !== null && finish_reason === "stop") {
-            try {
-                let jsonObject = JSON.parse(json_string);
-
-                // Extract each element and store it in a variable
-                let feature = jsonObject.Feature;
-                let prediction = jsonObject.Prediction;
-                let relationship = jsonObject.Relationship;
-                let condition = jsonObject.Condition;
-                let PossibleRelationships = jsonObject.PossibleRelationships;
-                let PossibleConditions = jsonObject.PossibleConditions;
-                PossibleRelationships.push(relationship)
-
-                if (feature === "ERROR" || prediction === "ERROR") {
-                    console.error("Improper feature/prediction detected");
-                }
-                else {
-                    // Log the variables to check the values
-                    console.log("Feature:", feature);
-                    console.log("Prediction:", prediction);
-                    console.log("Relationship:", relationship);
-                    console.log("Condition:", condition);
-                    console.log("Possible Relationships: ", PossibleRelationships);
-                    console.log("Possible Conditions: ", PossibleConditions)
-
-                    setParsedData({
-                        feature,
-                        prediction,
-                        relation: relationship,
-                        condition,
-                        PossibleRelationships,
-                        PossibleConditions
-                    });
-                }
-
-            } catch (error) {
-                console.error("Error parsing JSON:", error);
-            }
-            finally {
-                setIsLoading(false);
-            }
+        if (feature === "ERROR" || prediction === "ERROR") {
+          console.error("Improper feature/prediction detected");
         } else {
-            console.error("JSON string is null");
+          // Log the variables to check the values
+          console.log("Feature:", feature);
+          console.log("Prediction:", prediction);
+          console.log("Relationship:", relationship);
+          console.log("Condition:", condition);
+          console.log("Possible Relationships: ", PossibleRelationships);
+          console.log("Possible Conditions: ", PossibleConditions);
+
+          setParsedData({
+            feature,
+            prediction,
+            relation: relationship,
+            condition,
+            PossibleRelationships,
+            PossibleConditions,
+          });
         }
-    };
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      console.error("JSON string is null");
+    }
+  };
 
-    const handleSubmission = async () => {
-        parseInput(userInput);
-        setIsSubmitted(!isSubmitted);
-    };
+  const handleSubmission = async () => {
+    parseInput(userInput);
+    setIsSubmitted(!isSubmitted);
+  };
 
-    return (
-        <Paper style={{ padding: "15px" }}>
-            <Typography variant="h5" gutterBottom>
-                User Interpretation
-            </Typography>
-            <TextField
-                id="outlined-basic"
-                label="freeform text"
-                value={userInput}
-                onChange={(e) => setUserInput(e.target.value)}
-                multiline
-                rows={4}
-                fullWidth
-                disabled={!props.name.includes("Free")}
-            />
-            <div style={{ alignItems: "center" }}>
-                {props.name.includes("Free") && <Button
-                    variant="contained"
-                    color="primary"
-                    style={{ margin: "10px 5px" }}
-                    onClick={() => setIsSubmitted(!isSubmitted)}
-                >
-                    Clear
-                </Button>
-                }
-                <Button
-                    variant="contained"
-                    color="primary"
-                    style={{ margin: "10px 5px" }}
-                    onClick={handleSubmission}
-                >
-                    Check
-                </Button>
-            </div>
-            {isLoading ? (
-                <CircularProgress></CircularProgress>
-            ) : (
-                // [TODO: @aninuth update based on the hypo state]
-                isSubmitted && (
-                    <Paper className="parse-input" elevation={1}>
-                        {formatText(parsedData.feature, "feature")}
-                        {getSelection(
-                            "relation",
-                            selectedRelation,
-                            (k) => setSelectedRelation(k),
-                            parsedData.PossibleRelationships
-                        )}
-                        {formatText(parsedData.prediction, "prediction")}
-                        {getSelection("condition", selectedCondition, (k) => setSelectedCondition(k), parsedData.PossibleConditions)}
-                    </Paper>
-                )
-            )}
-        </Paper>
-    );
+  return (
+    <Paper style={{ padding: "15px" }}>
+      <Typography variant="h5" gutterBottom>
+        User Interpretation
+      </Typography>
+      <TextField
+        id="outlined-basic"
+        label="freeform text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        multiline
+        rows={4}
+        fullWidth
+        disabled={!props.name.includes("Free")}
+      />
+      <div style={{ alignItems: "center" }}>
+        {props.name.includes("Free") && (
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: "10px 5px" }}
+            onClick={() => setIsSubmitted(!isSubmitted)}
+          >
+            Clear
+          </Button>
+        )}
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ margin: "10px 5px" }}
+          onClick={handleSubmission}
+        >
+          Check
+        </Button>
+      </div>
+      {props.name.includes("Free") &&
+        (isLoading ? (
+          <CircularProgress></CircularProgress>
+        ) : (
+          // [TODO: @aninuth update based on the hypo state]
+          isSubmitted && (
+            <Paper className="parse-input" elevation={1}>
+              {formatText(parsedData.feature, "feature")}
+              {getSelection(
+                "relation",
+                selectedRelation,
+                (k) => setSelectedRelation(k),
+                parsedData.PossibleRelationships
+              )}
+              {formatText(parsedData.prediction, "prediction")}
+              {getSelection(
+                "condition",
+                selectedCondition,
+                (k) => setSelectedCondition(k),
+                parsedData.PossibleConditions
+              )}
+            </Paper>
+          )
+        ))}
+    </Paper>
+  );
 }
