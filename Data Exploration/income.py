29ccdfb0a22f2bc422db2dad8beb350c84a19226
Nick Hinds <nicholashinds2@gmail.com>
2024-07-18 11:52:08 -0500

    income wip

--- Data Exploration/income.py
+++ Data Exploration/income.py
@@ -12,3 +12,11 @@ shap_values = explainer(X)
 # %%
 shap.plots.scatter(shap_values[:, 'Age'],color=shap_values[:, 'Education-Num'])
 # %%
+import json
+data = {}
+data['shap_values'] = shap_values.values.tolist()
+data['feature_names'] = X.columns.tolist()
+data['feature_values'] = X.values.tolist()
+
+with open('shap_income.json', 'w') as outfile:
+    json.dump(data, outfile)
--- prototype/src/component/Explanation.tsx
+++ prototype/src/component/Explanation.tsx
@@ -1,125 +1,149 @@
 import { Paper, Typography } from "@mui/material";
 import shap_diabetes from "../assets/shap_diabetes.json";
+import shap_income from "../assets/shap_income_subset.json";
 
 import Swarm from "./Swarm";
 import Scatter from "./Scatter";
 import Bar from "./Bar";
 import { IHypo } from "../App";
-import { CASES } from '../const';
+import { CASES } from "../const";
 
-type props = typeof CASES[0] & {
-    isSubmitted: boolean;
-    hypo: IHypo | undefined
-}
+type props = (typeof CASES)[0] & {
+  isSubmitted: boolean;
+  hypo: IHypo | undefined;
+};
 
 export default function Explanation({ isSubmitted, initVis, hypo }: props) {
-    const featureName = "bmi",
-        featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
-        featureValues = shap_diabetes["feature_values"].map(
-            (row) => row[featureIndex]
-        ),
-        featureShapValues = shap_diabetes["shap_values"].map((row) => row[featureIndex]);
-
-
-    let initialVisualization;
-    switch (initVis) {
-        case "beeswarm":
-            initialVisualization = (
-                <Swarm
-                    xValues={featureShapValues}
-                    colorValues={featureValues}
-                    width={500}
-                    height={100}
-                    id="bmi"
-                />
-            );
-            break;
-        case "scatter":
-            initialVisualization = (
-                <Scatter
-                    yValues={featureShapValues}
-                    xValues={featureValues}
-                    width={400}
-                    height={300}
-                    id="bmi-scatter"
-                    offsets={[0, 0]}
-                />
-            );
-            break;
-        case "bar":
-            initialVisualization = (
-                <Bar
-                    allShapValues={shap_diabetes["shap_values"]}
-                    featureNames={shap_diabetes["feature_names"]}
-                    width={600}
-                    height={200}
-                    id="bmi-scatter"
-                    offsets={[0, 0]}
-                />
-            );
-            break;
-        default:
-            initialVisualization = (
-                <Swarm
-                    xValues={featureShapValues}
-                    colorValues={featureValues}
-                    width={500}
-                    height={100}
-                    id="bmi"
-                />)
-    }
-
-    // [TODO: additional visualizations should be updated based in hypothesis]
-    const additionalVisualizations = isSubmitted && (
-        <>
-            <Scatter
-                yValues={featureShapValues}
-                xValues={featureValues}
-                width={400}
-                height={300}
-                id="bmi-scatter"
-                offsets={[0, 150]}
-            />
-
-            <g>
-                <rect
-                    x={550}
-                    y={0}
-                    width={300}
-                    height={200}
-                    fill="white"
-                    stroke="black"
-                />
-                <text x={700} y={50} textAnchor="middle">
-                    Other Explanations
-                </text>
-            </g>
-            <g>
-                <rect
-                    x={450}
-                    y={250}
-                    width={400}
-                    height={200}
-                    fill="white"
-                    stroke="black"
-                />
-                <text x={600} y={300} textAnchor="middle">
-                    Other Explanations
-                </text>
-            </g>
-        </>
-    )
-
-    return (
-        <Paper style={{ padding: "15px" }}>
-            <Typography variant="h5" gutterBottom>
-                Visual Explanation
-            </Typography>
-            <svg className="swarm" width={900} height={500}>
-                {initialVisualization}
-
-                {additionalVisualizations}
-            </svg>
-        </Paper>
+  const featureName = "bmi",
+    featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
+    featureValues = shap_diabetes["feature_values"].map(
+      (row) => row[featureIndex]
+    ),
+    featureShapValues = shap_diabetes["shap_values"].map(
+      (row) => row[featureIndex]
     );
+
+  const incomeFeatureName = "Age",
+    incomeFeatureIndex =
+      shap_income["feature_names"].indexOf(incomeFeatureName),
+    incomeFeatureValues = shap_income["feature_values"].map(
+      (row) => row[incomeFeatureIndex]
+    ),
+    incomeFeatureShapValues = shap_income["shap_values"].map(
+      (row) => row[incomeFeatureIndex]
+    );
+
+  let initialVisualization;
+  switch (initVis) {
+    case "beeswarm":
+      initialVisualization = (
+        <Swarm
+          xValues={featureShapValues}
+          colorValues={featureValues}
+          width={1000}
+          height={500}
+          id="bmi"
+        />
+      );
+      break;
+    case "scatter":
+      initialVisualization = (
+        <Scatter
+          yValues={featureShapValues}
+          xValues={featureValues}
+          width={400}
+          height={300}
+          id="bmi-scatter"
+          offsets={[0, 0]}
+        />
+      );
+      break;
+    case "bar":
+      initialVisualization = (
+        <Bar
+          allShapValues={shap_diabetes["shap_values"]}
+          featureNames={shap_diabetes["feature_names"]}
+          width={600}
+          height={200}
+          id="bmi-scatter"
+          offsets={[0, 0]}
+        />
+      );
+      break;
+    case "beeswarm_income":
+      initialVisualization = (
+        <Swarm
+          xValues={incomeFeatureShapValues}
+          colorValues={incomeFeatureValues}
+          width={1000}
+          height={500}
+          id="Age"
+        />
+      );
+      break;
+    default:
+      initialVisualization = (
+        <Swarm
+          xValues={featureShapValues}
+          colorValues={featureValues}
+          width={500}
+          height={100}
+          id="bmi"
+        />
+      );
+  }
+
+  // [TODO: additional visualizations should be updated based in hypothesis]
+  const additionalVisualizations = isSubmitted && (
+    <>
+      <Scatter
+        yValues={featureShapValues}
+        xValues={featureValues}
+        width={400}
+        height={300}
+        id="bmi-scatter"
+        offsets={[0, 150]}
+      />
+
+      <g>
+        <rect
+          x={550}
+          y={0}
+          width={300}
+          height={200}
+          fill="white"
+          stroke="black"
+        />
+        <text x={700} y={50} textAnchor="middle">
+          Other Explanations
+        </text>
+      </g>
+      <g>
+        <rect
+          x={450}
+          y={250}
+          width={400}
+          height={200}
+          fill="white"
+          stroke="black"
+        />
+        <text x={600} y={300} textAnchor="middle">
+          Other Explanations
+        </text>
+      </g>
+    </>
+  );
+
+  return (
+    <Paper style={{ padding: "15px" }}>
+      <Typography variant="h5" gutterBottom>
+        Visual Explanation
+      </Typography>
+      <svg className="swarm" width={900} height={500}>
+        {initialVisualization}
+
+        {additionalVisualizations}
+      </svg>
+    </Paper>
+  );
 }
