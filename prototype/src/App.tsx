a0d51ea3a15e03d85f7b56277d2fc62e2996aba1
wangqianwen0418 <wangqianwen0418@163.com>
2024-08-14 20:55:10 -0500

    refactor free text

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -10,136 +10,139 @@ import "./App.css";
 
 import IconButton from "@mui/material/IconButton";
 import {
-  Menu as MenuIcon,
-  TroubleshootOutlined as TroubleShootIcon,
-  Sync as SyncIcon,
+    Menu as MenuIcon,
+    TroubleshootOutlined as TroubleShootIcon,
+    Sync as SyncIcon,
 } from "@mui/icons-material";
 import {
-  Toolbar,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Box,
-  Paper,
-  Typography,
+    Toolbar,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Box,
+    Paper,
+    Typography,
 } from "@mui/material";
 
 export interface IHypo {
-  freetext: string;
-  features: string[];
-  featureState: string[];
-  attribution: string;
-  relation: string;
-  prediction: string;
-  condition: string;
-  constant: string;
-  possibleRelations: string[];
-  possibleConditions: string[];
-  category: number;
+    freetext: string;
+    features: string[];
+    featureState: string[];
+    attribution: string;
+    relation: string;
+    prediction: string;
+    condition: string;
+    constant: string;
+    possibleRelations: string[];
+    possibleConditions: string[];
+    category: number;
 }
 
 function App(appProps: TCase) {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [open, setOpen] = useState(false); // sider drawer
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [open, setOpen] = useState(false); // sider drawer
 
-  const [insight, setInsight] = useState<TInsight>(appProps.insight);
+    const [insight, setInsight] = useState<TInsight>(appProps.insight);
+    const [freetext, setFreetext] = useState<string>(appProps.userText);
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={() => setOpen(false)}>
-      <List>
-        <ListItem key="drawerheader">
-          <ListItemText
-            primary={
-              <span
-                style={{
-                  fontSize: "20px",
-                  textAlign: "center",
-                  marginLeft: 15,
-                }}
-              >
-                Cases
-              </span>
-            }
-          />
-        </ListItem>
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={() => setOpen(false)}>
+            <List>
+                <ListItem key="drawerheader">
+                    <ListItemText
+                        primary={
+                            <span
+                                style={{
+                                    fontSize: "20px",
+                                    textAlign: "center",
+                                    marginLeft: 15,
+                                }}
+                            >
+                                Cases
+                            </span>
+                        }
+                    />
+                </ListItem>
 
-        {CASES.map((c) => (
-          <ListItem key={c.name} disablePadding>
-            <ListItemButton href={c.href}>
-              <ListItemIcon>
-                <TroubleShootIcon />
-              </ListItemIcon>
-              <ListItemText primary={c.name} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+                {CASES.map((c) => (
+                    <ListItem key={c.name} disablePadding>
+                        <ListItemButton href={c.href}>
+                            <ListItemIcon>
+                                <TroubleShootIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={c.name} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
 
-  const Header = (
-    <Grid item xs={12}>
-      <Toolbar
-        sx={{ backgroundColor: "black", fontSize: "28px", color: "white" }}
-      >
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={() => setOpen(true)}
-          edge="start"
-          // sx={{ mr: 2, ...(open && { display: 'none' }) }}
-        >
-          <MenuIcon />
-        </IconButton>
-        Are you interpreting correctly?
-      </Toolbar>
-    </Grid>
-  );
+    const Header = (
+        <Grid item xs={12}>
+            <Toolbar
+                sx={{ backgroundColor: "black", fontSize: "28px", color: "white" }}
+            >
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={() => setOpen(true)}
+                    edge="start"
+                // sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                Are you interpreting correctly?
+            </Toolbar>
+        </Grid>
+    );
 
-  return (
-    <Grid container justifyContent="center">
-      {Header}
-      <Drawer open={open} onClose={() => setOpen(false)}>
-        {DrawerList}
-      </Drawer>
+    return (
+        <Grid container justifyContent="center">
+            {Header}
+            <Drawer open={open} onClose={() => setOpen(false)}>
+                {DrawerList}
+            </Drawer>
 
-      <Grid item xs={10}>
-        <Paper style={{ padding: "15px" }}>
-          <Typography variant="h5" gutterBottom>
-            {" "}
-            ML Model and Dataset{" "}
-            {appProps.name.includes("Free") && <SyncIcon />}
-          </Typography>
-          <p style={{ margin: "0px 5px" }}>
-            Each instance in the dataset corresponds to a patient, characterized
-            by values for 10 distinct features.
-            <br />
-            This machine learning model predicts the progression of diabetes in
-            patients using these 10 features.
-          </p>
-        </Paper>
-      </Grid>
-      <Grid item xs={4} className="App-body">
-        <Interpretation
-          isSubmitted={isSubmitted}
-          setIsSubmitted={setIsSubmitted}
-          {...appProps}
-          insight={insight}
-          setInsight={setInsight}
-        />
-      </Grid>
-      <Grid item xs={7} className="App-body">
-        <Explanation
-          isSubmitted={isSubmitted}
-          insight={insight}
-          {...appProps}
-        />
-      </Grid>
-    </Grid>
-  );
+            <Grid item xs={10}>
+                <Paper style={{ padding: "15px" }}>
+                    <Typography variant="h5" gutterBottom>
+                        {" "}
+                        ML Model and Dataset{" "}
+                        {appProps.name.includes("Free") && <SyncIcon />}
+                    </Typography>
+                    <p style={{ margin: "0px 5px" }}>
+                        Each instance in the dataset corresponds to a patient, characterized
+                        by values for 10 distinct features.
+                        <br />
+                        This machine learning model predicts the progression of diabetes in
+                        patients using these 10 features.
+                    </p>
+                </Paper>
+            </Grid>
+            <Grid item xs={4} className="App-body">
+                <Interpretation
+                    isSubmitted={isSubmitted}
+                    setIsSubmitted={setIsSubmitted}
+                    {...appProps}
+                    userText={freetext}
+                    setUserText={setFreetext}
+                    insight={insight}
+                    setInsight={setInsight}
+                />
+            </Grid>
+            <Grid item xs={7} className="App-body">
+                <Explanation
+                    isSubmitted={isSubmitted}
+                    insight={insight}
+                    {...appProps}
+                />
+            </Grid>
+        </Grid>
+    );
 }
 
 export default App;
--- prototype/src/component/Interpretation.tsx
+++ prototype/src/component/Interpretation.tsx
@@ -1,136 +1,138 @@
 import {
-  Button,
-  Paper,
-  TextField,
-  Typography,
-  CircularProgress,
-  Modal,
-  Box,
+    Button,
+    Paper,
+    TextField,
+    Typography,
+    CircularProgress,
+    Modal,
+    Box,
 } from "@mui/material";
 import React from "react";
 
 import "./Interpretation.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import shapData from "../assets/shap_diabetes.json";
-import { IHypo } from "../App";
-import { CASES, TInsight, GenerateTextTemplates, TCase } from "../const";
+import { TInsight, GenerateTextTemplates, TCase } from "../const";
 import { generatePrompt, parseInput } from "../const/prompt";
 
 type props = TCase & {
-  isSubmitted: boolean;
-  setIsSubmitted: (k: boolean) => void;
-  insight: TInsight;
-  setInsight: (k: TInsight) => void;
+    isSubmitted: boolean;
+    setIsSubmitted: (k: boolean) => void;
+    userText: string;
+    setUserText: (k: string) => void;
+    insight: TInsight;
+    setInsight: (k: TInsight) => void;
 };
 
 export default function Interpretation(props: props) {
-  const {
-    isSubmitted,
-    name: caseName,
-    setIsSubmitted,
-    insight,
-    setInsight,
-    userText,
-  } = props;
+    const {
+        isSubmitted,
+        name: caseName,
+        setIsSubmitted,
+        insight,
+        setInsight,
+        userText,
+        setUserText
+    } = props;
 
-  const [modalVisible, setModalVisible] = useState(caseName.includes("Free"));
-  const [isLoading, setIsLoading] = useState(false); // New loading state
-  const [apiKey, setApiKey] = useState("");
+    const [modalVisible, setModalVisible] = useState(caseName.includes("Free"));
+    const [isLoading, setIsLoading] = useState(false); // New loading state
+    const [apiKey, setApiKey] = useState("");
 
-  const handleSubmission = async () => {
-    if (insight == undefined) {
-      setIsLoading(true); // Start loading
-      const prompt = generatePrompt(
-        shapData.feature_names,
-        shapData.prediction_name
-      );
-      const parsedInput = await parseInput(userText, apiKey, prompt);
-      setInsight(parsedInput);
-      setIsLoading(false); // Stop loading
-    }
-    setIsSubmitted(true);
-  };
+    const handleSubmission = async () => {
+        if (insight == undefined) {
+            setIsLoading(true); // Start loading
+            const prompt = generatePrompt(
+                shapData.feature_names,
+                shapData.prediction_name
+            );
+            const parsedInput = await parseInput(userText, apiKey, prompt);
+            setInsight(parsedInput);
+            setIsLoading(false); // Stop loading
+        }
+        setIsSubmitted(true);
+    };
 
-  return (
-    <>
-      <Paper style={{ padding: "15px" }}>
-        <Typography variant="h5" gutterBottom>
-          User Interpretation
-        </Typography>
-        <TextField
-          id="outlined-basic"
-          label="e.g., a high bmi leads to large diabete progression"
-          value={userText}
-          //   onChange={(e) =>
-          //     caseName.includes("Free") && setUserInput(e.target.value)
-          //   }
-          multiline
-          rows={4}
-          fullWidth
-        />
+    return (
+        <>
+            <Paper style={{ padding: "15px" }}>
+                <Typography variant="h5" gutterBottom>
+                    User Interpretation
+                </Typography>
+                <TextField
+                    id="outlined-basic"
+                    label="e.g., a high bmi leads to large diabete progression"
+                    value={userText}
+                    onChange={(e) =>
+                        caseName.includes("Free") && setUserText(e.target.value)
+                    }
+                    multiline
+                    rows={4}
+                    fullWidth
+                />
 
-        <div style={{ alignItems: "center" }}>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ margin: "10px 5px" }}
-            onClick={() => setIsSubmitted(!isSubmitted)}
-          >
-            Clear
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ margin: "10px 5px" }}
-            onClick={handleSubmission}
-          >
-            Submit
-          </Button>
-        </div>
-        {isLoading ? (
-          <CircularProgress></CircularProgress>
-        ) : (
-          isSubmitted && (
-            <Paper className="parse-input" elevation={1}>
-              {GenerateTextTemplates(insight)}
+                <div style={{ alignItems: "center" }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ margin: "10px 5px" }}
+                        onClick={() => setIsSubmitted(!isSubmitted)}
+                    >
+                        Clear
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ margin: "10px 5px" }}
+                        onClick={handleSubmission}
+                    >
+                        Submit
+                    </Button>
+                </div>
+                {isLoading ? (
+                    <CircularProgress></CircularProgress>
+                ) : (
+                    isSubmitted && (
+                        <Paper className="parse-input" elevation={1}>
+                            {GenerateTextTemplates(insight)}
+                        </Paper>
+                    )
+                )}
             </Paper>
-          )
-        )}
-      </Paper>
-      <Modal open={modalVisible} onClose={() => {}}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <TextField
-            id="api-key"
-            label="Enter your OpenAI API key here"
-            value={apiKey}
-            onChange={(e) => setApiKey(e.target.value)}
-            rows={1}
-            fullWidth
-            style={{ marginTop: "15px" }}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ margin: "10px 5px" }}
-            onClick={() => setModalVisible(false)}
-          >
-            Submit
-          </Button>
-        </Box>
-      </Modal>
-    </>
-  );
+            <Modal open={modalVisible} onClose={() => { }}>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 400,
+                        bgcolor: "background.paper",
+                        border: "2px solid #000",
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                >
+                    <TextField
+                        id="api-key"
+                        label="Enter your OpenAI API key here"
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                        rows={1}
+                        fullWidth
+                        style={{ marginTop: "15px" }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ margin: "10px 5px" }}
+                        onClick={() => setModalVisible(false)}
+                    >
+                        Submit
+                    </Button>
+                </Box>
+            </Modal>
+        </>
+    );
 }
