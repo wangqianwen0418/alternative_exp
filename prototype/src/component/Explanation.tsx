15bcbefc60a608a840ead7db12ca9206791ee792
wangqianwen0418 <wangqianwen0418@163.com>
2024-08-21 18:17:30 -0500

    data clearing

--- prototype/src/component/Explanation.tsx
+++ prototype/src/component/Explanation.tsx
@@ -1,139 +1,137 @@
 import { Paper, Typography } from "@mui/material";
 import shap_diabetes from "../assets/shap_diabetes.json";
 import { useState } from "react";
+import React from "react";
 
 import Swarm from "./Swarm";
 import Scatter from "./Scatter";
 import Bar from "./Bar";
-import PCP from "./PCP";
-import { CASES } from "../util/cases";
-import { TInsight } from "../util/types";
 import { useAtom } from "jotai";
 import { initVisAtom, insightAtom, isSubmittedAtom } from "../store";
 
 export default function Explanation() {
-  const [isSubmitted, setIsSubmitted] = useAtom(isSubmittedAtom);
-  const [insight, setInsight] = useAtom(insightAtom);
-  const [initVis, setInitVis] = useAtom(initVisAtom);
-  const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
+    const [isSubmitted] = useAtom(isSubmittedAtom);
+    const [insight, setInsight] = useAtom(insightAtom);
+    const [initVis] = useAtom(initVisAtom);
+    const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
 
-  const featureName = "bmi",
-    featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
-    featureValues = shap_diabetes["feature_values"].map(
-      (row) => row[featureIndex]
-    ),
-    featureShapValues = shap_diabetes["shap_values"].map(
-      (row) => row[featureIndex]
+    const featureName = "bmi",
+        featureIndex = shap_diabetes["feature_names"].indexOf(featureName),
+        featureValues = shap_diabetes["feature_values"].map(
+            (row) => row[featureIndex]
+        ),
+        featureShapValues = shap_diabetes["shap_values"].map(
+            (row) => row[featureIndex]
+        );
+
+    let initialVisualization;
+    switch (initVis) {
+        case "beeswarm":
+            initialVisualization = (
+                <Swarm
+                    xValues={featureShapValues}
+                    colorValues={featureValues}
+                    width={500}
+                    height={100}
+                    id="bmi"
+                    selectedIndices={selectedIndices}
+                    setSelectedIndices={setSelectedIndices}
+                />
+            );
+            break;
+        case "scatter":
+            initialVisualization = (
+                <Scatter
+                    yValues={featureShapValues}
+                    xValues={featureValues}
+                    width={400}
+                    height={300}
+                    id="bmi-scatter"
+                    offsets={[0, 0]}
+                    selectedIndices={selectedIndices}
+                    setSelectedIndices={setSelectedIndices}
+                />
+            );
+            break;
+        case "bar":
+            initialVisualization = (
+                <Bar
+                    allShapValues={shap_diabetes["shap_values"].slice(0, 100)}
+                    featureNames={shap_diabetes["feature_names"].slice(0, 100)}
+                    width={600}
+                    height={200}
+                    id="bmi-scatter"
+                    offsets={[0, 0]}
+                />
+            );
+            break;
+        default:
+            initialVisualization = (
+                <Swarm
+                    xValues={featureShapValues}
+                    colorValues={featureValues}
+                    width={500}
+                    height={100}
+                    id="bmi"
+                    selectedIndices={selectedIndices}
+                    setSelectedIndices={setSelectedIndices}
+                />
+            );
+    }
+
+    // [TODO: additional visualizations should be updated based in hypothesis]
+    const additionalVisualizations = isSubmitted && (
+        <>
+            <Scatter
+                yValues={featureShapValues}
+                xValues={featureValues}
+                width={400}
+                height={300}
+                id="bmi-scatter"
+                offsets={[0, 150]}
+                selectedIndices={selectedIndices}
+                setSelectedIndices={setSelectedIndices}
+            />
+
+            <g>
+                <rect
+                    x={550}
+                    y={0}
+                    width={300}
+                    height={200}
+                    fill="white"
+                    stroke="black"
+                />
+                <text x={700} y={50} textAnchor="middle">
+                    Other Explanations
+                </text>
+            </g>
+            <g>
+                <rect
+                    x={450}
+                    y={250}
+                    width={400}
+                    height={200}
+                    fill="white"
+                    stroke="black"
+                />
+                <text x={600} y={300} textAnchor="middle">
+                    Other Explanations
+                </text>
+            </g>
+        </>
     );
 
-  let initialVisualization;
-  switch (initVis) {
-    case "beeswarm":
-      initialVisualization = (
-        <Swarm
-          xValues={featureShapValues}
-          colorValues={featureValues}
-          width={500}
-          height={100}
-          id="bmi"
-          selectedIndices={selectedIndices}
-          setSelectedIndices={setSelectedIndices}
-        />
-      );
-      break;
-    case "scatter":
-      initialVisualization = (
-        <Scatter
-          yValues={featureShapValues}
-          xValues={featureValues}
-          width={400}
-          height={300}
-          id="bmi-scatter"
-          offsets={[0, 0]}
-          selectedIndices={selectedIndices}
-          setSelectedIndices={setSelectedIndices}
-        />
-      );
-      break;
-    case "bar":
-      initialVisualization = (
-        <Bar
-          allShapValues={shap_diabetes["shap_values"].slice(0, 100)}
-          featureNames={shap_diabetes["feature_names"].slice(0, 100)}
-          width={600}
-          height={200}
-          id="bmi-scatter"
-          offsets={[0, 0]}
-        />
-      );
-      break;
-    default:
-      initialVisualization = (
-        <Swarm
-          xValues={featureShapValues}
-          colorValues={featureValues}
-          width={500}
-          height={100}
-          id="bmi"
-          selectedIndices={selectedIndices}
-          setSelectedIndices={setSelectedIndices}
-        />
-      );
-  }
+    return (
+        <Paper style={{ padding: "15px" }}>
+            <Typography variant="h5" gutterBottom>
+                Visual Explanation
+            </Typography>
+            <svg className="swarm" width={900} height="70vh">
+                {initialVisualization}
 
-  // [TODO: additional visualizations should be updated based in hypothesis]
-  const additionalVisualizations = isSubmitted && (
-    <>
-      <Scatter
-        yValues={featureShapValues}
-        xValues={featureValues}
-        width={400}
-        height={300}
-        id="bmi-scatter"
-        offsets={[0, 150]}
-        selectedIndices={selectedIndices}
-        setSelectedIndices={setSelectedIndices}
-      />
-
-      <g>
-        <rect
-          x={550}
-          y={0}
-          width={300}
-          height={200}
-          fill="white"
-          stroke="black"
-        />
-        <text x={700} y={50} textAnchor="middle">
-          Other Explanations
-        </text>
-      </g>
-      <g>
-        <rect
-          x={450}
-          y={250}
-          width={400}
-          height={200}
-          fill="white"
-          stroke="black"
-        />
-        <text x={600} y={300} textAnchor="middle">
-          Other Explanations
-        </text>
-      </g>
-    </>
-  );
-
-  return (
-    <Paper style={{ padding: "15px" }}>
-      <Typography variant="h5" gutterBottom>
-        Visual Explanation
-      </Typography>
-      <svg className="swarm" width={900} height="70vh">
-        {initialVisualization}
-
-        {isSubmitted && additionalVisualizations}
-      </svg>
-    </Paper>
-  );
+                {isSubmitted && additionalVisualizations}
+            </svg>
+        </Paper>
+    );
 }
--- prototype/src/component/UserResponse.tsx
+++ prototype/src/component/UserResponse.tsx
@@ -3,97 +3,96 @@ import { Button, Typography, Paper, RadioGroup, Radio } from "@mui/material";
 import { QuestionList } from "../util/questionList";
 import { useAtom } from "jotai";
 import {
-  questionIndexAtom,
-  insightAtom,
-  freeTextAtom,
-  initVisAtom,
-  pageNameAtom,
-  isSubmittedAtom,
+    questionIndexAtom,
+    insightAtom,
+    freeTextAtom,
+    initVisAtom,
+    pageNameAtom,
+    isSubmittedAtom,
 } from "../store";
 import Selection from "./webUtil/Selection";
 
 const confidenceOptions = [
-  "please select",
-  "0 not confidence at all",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6 very confident",
+    "please select",
+    "1 not confidence at all",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6 very confident",
 ];
 export default function UserResponse() {
-  const [questionIndex, setQuestionIndex] = useAtom(questionIndexAtom);
-  const [, setInsight] = useAtom(insightAtom);
-  const [, setFreetext] = useAtom(freeTextAtom);
-  const [, setInitVis] = useAtom(initVisAtom);
-  const [, setName] = useAtom(pageNameAtom);
-  const [, setIsSubmitted] = useAtom(isSubmittedAtom);
+    const [questionIndex, setQuestionIndex] = useAtom(questionIndexAtom);
+    const [, setInsight] = useAtom(insightAtom);
+    const [, setFreetext] = useAtom(freeTextAtom);
+    const [, setInitVis] = useAtom(initVisAtom);
+    const [, setName] = useAtom(pageNameAtom);
+    const [, setIsSubmitted] = useAtom(isSubmittedAtom);
 
-  const [userAnswer, setUserAnswer] = React.useState<
-    "yes" | "no" | undefined
-  >();
-  const [confidence, setConfidence] = React.useState<string>(
-    confidenceOptions[0]
-  );
+    const [userAnswer, setUserAnswer] = React.useState<
+        "yes" | "no" | undefined
+    >();
+    const [confidence, setConfidence] = React.useState<string>(
+        confidenceOptions[0]
+    );
 
-  const onSubmit = () => {
-    // recording(userAnswer, confidence) commit the results to the database
-    setUserAnswer(undefined);
-    setConfidence(confidenceOptions[0]);
-    setIsSubmitted(false);
+    const onSubmit = () => {
+        // recording(userAnswer, confidence) commit the results to the database
+        setUserAnswer(undefined);
+        setConfidence(confidenceOptions[0]);
+        setIsSubmitted(false);
 
-    setFreetext(QuestionList[questionIndex + 1].userText);
-    setInsight(QuestionList[questionIndex + 1].insight);
-    setInitVis(QuestionList[questionIndex + 1].initVis);
-    setName(QuestionList[questionIndex + 1].pageName);
+        setFreetext(QuestionList[questionIndex + 1].userText);
+        setInsight(QuestionList[questionIndex + 1].insight);
+        setInitVis(QuestionList[questionIndex + 1].initVis);
+        setName(QuestionList[questionIndex + 1].pageName);
 
-    setQuestionIndex((questionIndex) => questionIndex + 1);
-  };
-  return (
-    <Paper style={{ padding: "15px 20px", marginTop: "10px" }}>
-      <Typography variant="h5" gutterBottom sx={{ fontWeight: 800 }}>
-        Please Respond Here
-      </Typography>
-      <span>
-        <b>Q1:</b>Is the above interpretation accurate?
-      </span>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <Radio
-          checked={userAnswer == "yes"}
-          onChange={() => setUserAnswer("yes")}
-        />{" "}
-        <span style={{ margin: "auto 0" }}>Yes, accurate</span>
-        <Radio
-          checked={userAnswer == "no"}
-          onChange={() => setUserAnswer("no")}
-        />{" "}
-        <span style={{ margin: "auto 0" }}>No, inaccurate </span>
-      </RadioGroup>
+        setQuestionIndex((questionIndex) => questionIndex + 1);
+    };
+    return (
+        <Paper style={{ padding: "15px 20px", marginTop: "10px" }}>
+            <Typography variant="h5" gutterBottom sx={{ fontWeight: 800 }}>
+                Please Respond Here
+            </Typography>
+            <span>
+                <b>Q1:</b>Is the above interpretation accurate?
+            </span>
+            <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+            >
+                <Radio
+                    checked={userAnswer == "yes"}
+                    onChange={() => setUserAnswer("yes")}
+                />{" "}
+                <span style={{ margin: "auto 0" }}>Yes, accurate</span>
+                <Radio
+                    checked={userAnswer == "no"}
+                    onChange={() => setUserAnswer("no")}
+                />{" "}
+                <span style={{ margin: "auto 0" }}>No, inaccurate </span>
+            </RadioGroup>
 
-      <div style={{ display: "flex" }}>
-        <span style={{ margin: "auto 0" }}>
-          <b>Q2:</b>Please rate your confidence{" "}
-        </span>
-        <Selection
-          label=""
-          value={confidence}
-          handleChange={setConfidence}
-          options={confidenceOptions}
-        />
-      </div>
-      <br />
-      <Button
-        variant="contained"
-        disabled={userAnswer == undefined || confidence == confidenceOptions[0]}
-        onClick={() => onSubmit()}
-      >
-        {questionIndex < QuestionList.length - 1 ? "Next" : "Submit"}
-      </Button>
-    </Paper>
-  );
+            <div style={{ display: "flex" }}>
+                <span style={{ margin: "auto 0" }}>
+                    <b>Q2:</b>Please rate your confidence{" "}
+                </span>
+                <Selection
+                    label=""
+                    value={confidence}
+                    handleChange={setConfidence}
+                    options={confidenceOptions}
+                />
+            </div>
+            <br />
+            <Button
+                variant="contained"
+                disabled={userAnswer == undefined || confidence == confidenceOptions[0]}
+                onClick={() => onSubmit()}
+            >
+                {questionIndex < QuestionList.length - 1 ? "Next" : "Submit"}
+            </Button>
+        </Paper>
+    );
 }
